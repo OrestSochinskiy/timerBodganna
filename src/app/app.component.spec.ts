7c89ae82a5f982ad04d17addb944bc0461907add
OrestSochinskiy <sochinskiyorest@gmail.com>
2021-12-14 00:52:19 +0200

    done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'timerBodganna'`, () => {
+  it(`should have as title 'count-down-timer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('timerBodganna');
+    expect(app.title).toEqual('count-down-timer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('timerBodganna app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('count-down-timer app is running!');
   });
 });
